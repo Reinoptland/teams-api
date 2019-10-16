71938dd8178a1f0f0fa7528157e5ebc25518e30d
Wouter de Vos <wouter@codaisseur.com>
2019-10-16 17:46:04 +0200

    Require database

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const app = express();
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 4000;
 
+require('./db');
+
 app.listen(port, () => {
-  console.log(`app is listening on port ${port}`);
+  console.log(`App is listening on port ${port}`);
 });
