b5ea41a6656c9152b965bd112bc02cb6f8f2df1a
reinoptland <reinoptland@gmail.com>
2019-10-22 13:58:32 +0200

    User model

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,24 +1,45 @@
-const { toData } = require('./jwt')
+// Import toData, that decrypts our token and gives the encrypted data
+const { toData } = require("./jwt");
 
+// TODO:
+
+// Authenticate user by checking the information in the token
+// X Make User Model
+// - Make a way for a user to sign up
+//    - Backend
+//    - Fronted
+// - Make a way for a user to login (this is when we give them a token)
+//    - Backend
+//    - Frontend
+// - Make the user send us their token in requests that require authorization
+//    - Frontend
+
+// Update the middleware
+// - Change this auth middleware to check database iif this user exists
+// - Add the information from the user to the "req" object (so we can use it in the next step)
+
+// function with 3 parameters, req, res, next <-- when this middleware is "done" we call next()
 const authMiddleWare = (req, res, next) => {
-    const auth = req.headers.authorization && req.headers.authorization.split(' ')
-    console.log('SPLIT:', auth)
+  // do we an auth header, if so split it
+  const auth =
+    req.headers.authorization && req.headers.authorization.split(" "); // 'Bearer `ldsvhasdjbvs`.dv'
+  console.log("SPLIT:", auth);
 
-    // is auth something && is the first element a string "Bearer" && do we have a token
-    if (auth && auth[0] === 'Bearer' && auth[1]) {
-      // verify the token and get me the information inside (toData(auth[1]))
-      const data = toData(auth[1])
+  // is auth something && is the first element a string "Bearer" && do we have a token
+  // auth[1] should be a token
+  if (auth && auth[0] === "Bearer" && auth[1]) {
+    // verify the token and get me the information inside (toData(auth[1]))
+    const data = toData(auth[1]);
     //   res.send({
     //     message: 'Thanks for visiting the secret endpoint.',
     //     data
     //   })
-      next()
-    }
-    else {
-      res.status(401).send({
-        message: 'Please supply some valid credentials'
-      })
-    }
-}
+    next();
+  } else {
+    res.status(401).send({
+      message: "Please supply some valid credentials"
+    });
+  }
+};
 
-module.exports = authMiddleWare
+module.exports = authMiddleWare;
--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 // middlewares
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const bodyParserMiddleWare = bodyParser.json()
-const corsMiddleWare = cors()
-const authMiddleWare = require('./auth/middleware')
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bodyParserMiddleWare = bodyParser.json();
+const corsMiddleWare = cors();
+const authMiddleWare = require("./auth/middleware");
 
 // Routers
-const teamRouter = require('./team/router');
-const playerRouter = require('./player/router')
-const authRouter = require('./auth/router')
+const teamRouter = require("./team/router");
+const playerRouter = require("./player/router");
+const authRouter = require("./auth/router");
 
 // Models & DB
-const Team = require('./team/model')
-const Player = require('./player/model')
-const db = require('./db')
+const Team = require("./team/model");
+const Player = require("./player/model");
+const User = require("./user/model");
+const db = require("./db");
 
 // Init
 const app = express();
 const port = process.env.PORT || 4000;
 
-
-
 // If req.body is undefined
 // - use bodyparser
 // - make sure to app.use(bodyparser) before doing app.use(blablRouter)
-// - order matters here (wtf?) -> probably for a good reason 
+// - order matters here (wtf?) -> probably for a good reason
 
 const loggingMiddleWare = (req, res, next) => {
-  console.log('I am a middleware', Date.now())
-  next() // everything is ok -> next()
-}
+  console.log("I am a middleware", Date.now());
+  next(); // everything is ok -> next()
+};
 
 app
   // use auth middleware for entire routers (maybe a bit heavy handed)
@@ -46,11 +45,9 @@ app
     console.log(`App is listening on port ${port}`);
   });
 
-
-db
-  .sync({ force: true })
+db.sync({ force: true })
   .then(() => {
-    console.log('Database schema has been updated.');
+    console.log("Database schema has been updated.");
 
     // simple seeding script
     // const team = Team.create({ name: 'Egel'})
@@ -58,33 +55,32 @@ db
     // return Promise.all([team, team2])
 
     // Script that iterates over arrays and creates rows in the database for them
-    const teamNames = ['Egel', 'Das', 'Eagle', 'Pinguin']
-    
-    const teams = teamNames.map(teamName => Team.create({ name: teamName}))
-    return Promise.all(teams)
+    const teamNames = ["Egel", "Das", "Eagle", "Pinguin"];
+
+    const teams = teamNames.map(teamName => Team.create({ name: teamName }));
+    return Promise.all(teams);
   })
   .then(() => {
     const players = [
-      { name: 'Mimi', number: 4, teamId: 1},
-      { name: 'Wouter', number: 1, teamId: 2},
-      { name: 'David', number: 9, teamId: 3},
-      { name: 'Bram', number: 8, teamId: 4},
-      { name: 'Lisa', number: 10, teamId: 1},
-      { name: 'Miloud', number: 2, teamId: 2},
-      { name: 'Violeta', number: 3, teamId: 3},
-      { name: 'Johan', number: 5, teamId: 4},
-      { name: 'Danny', number: 6, teamId: 3},
-      { name: 'Rembert', number: 7, teamId: 2},
-      { name: 'Kelley', number: 10, teamId: 1},
-      { name: 'Jeroen', number: 12, teamId: 4},
-      { name: 'Rein', number: 11, teamId: 2},
-    ]
+      { name: "Mimi", number: 4, teamId: 1 },
+      { name: "Wouter", number: 1, teamId: 2 },
+      { name: "David", number: 9, teamId: 3 },
+      { name: "Bram", number: 8, teamId: 4 },
+      { name: "Lisa", number: 10, teamId: 1 },
+      { name: "Miloud", number: 2, teamId: 2 },
+      { name: "Violeta", number: 3, teamId: 3 },
+      { name: "Johan", number: 5, teamId: 4 },
+      { name: "Danny", number: 6, teamId: 3 },
+      { name: "Rembert", number: 7, teamId: 2 },
+      { name: "Kelley", number: 10, teamId: 1 },
+      { name: "Jeroen", number: 12, teamId: 4 },
+      { name: "Rein", number: 11, teamId: 2 }
+    ];
 
-    const playerPromises = players.map((player) => Player.create(player))
-    return Promise.all(playerPromises)
+    const playerPromises = players.map(player => Player.create(player));
+    return Promise.all(playerPromises);
   })
   .catch(console.error);
-
 
 /* 
 
